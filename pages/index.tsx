f25e8bf9cfbb0ffef8cbcde1d17b494a2f071430
MarufPulok <maruf12@student.sust.edu>
2023-07-04 17:46:26 +0600

    contact section start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Me from "@/components/Me";
@@ -6,7 +7,6 @@ import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 import { Inter } from "next/font/google";
 import Head from "next/head";
-import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +38,9 @@ export default function Home() {
         <Projects/>
       </section>
       {/* Contact Me */}
+      <section id="contact" className="snap-start">
+        <Contact/>
+      </section>
     </div>
   );
 }
